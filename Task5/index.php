97a8a15f1892bdb018269ef3dfd525e78a055880
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-04-30 18:31:55 +0300

    Update index.php

--- Task5/index.php
+++ Task5/index.php
@@ -65,7 +65,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $values['GENDER'] = empty($_COOKIE['GENDER_value']) ? '' : $_COOKIE['GENDER_value'];
     $values['BIOGRAFY'] = empty($_COOKIE['BIOGRAFY_value']) ? '' : $_COOKIE['BIOGRAFY_value'];
     $values['Lang_Prog'] = empty($_COOKIE['Lang_Prog_value']) ? array() : unserialize($_COOKIE['Lang_Prog_value']);
-    include('form4.php');
+    include('form.php');
     // Завершаем работу скрипта.
     exit();
   }

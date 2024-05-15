e85b5c4762a29afcc40df44bd96a789561c9a130
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-16 01:31:20 +0300

    Update form_patients.php

--- SummerPractice/PATIENTS/form_patients.php
+++ SummerPractice/PATIENTS/form_patients.php
@@ -3,6 +3,10 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
+        <?php 
+            $v = rand();
+            echo "<link rel='stylesheet' type='text/css' href='../style.css?v=$v' media='screen' />";
+        ?>
         <link rel="stylesheet" href="style.css" >
         <title>Добавить пациента</title>
         <style>

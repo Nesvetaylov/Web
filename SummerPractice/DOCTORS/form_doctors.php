f680206cc554196774cc93f02b4e19893dfe7f07
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-16 01:25:32 +0300

    Update form_doctors.php

--- SummerPractice/DOCTORS/form_doctors.php
+++ SummerPractice/DOCTORS/form_doctors.php
@@ -3,7 +3,12 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta charset="UTF-8">
-        <link rel="stylesheet" href="style.css" >
+        <?php 
+            $v = rand();
+            echo "<link rel='stylesheet' type='text/css' href='../style.css?v=$v' media='screen' />";
+            echo "<script>var selectedForm='$selectedForm'</script>"; 
+        ?>
+        <!-- <link rel="stylesheet" href="style.css" > -->
         <title>Добавить врача</title>
         <style>
             .error {

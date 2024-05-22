4d7964bd6734348b3aedf1dc38aab39603b5e971
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-22 23:48:41 +0300

    Update first_query.php

--- SummerPractice/QUERIES/first_query.php
+++ SummerPractice/QUERIES/first_query.php
@@ -1,22 +1,24 @@
 <?php
+header('Content-Type: application/json; charset=UTF-8');
 
 include("../Secret.php");
 $username = username;
 $password = password;
 
 try {
-    $conn = new PDO( "mysql:host=localhost;dbname=$username", $username, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
+    $conn = new PDO(
+        "mysql:host=localhost;dbname=$username",
+        $username,
+        $password,
+        [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
     $query = "select * from DOCTORS where SPECIALITY_DOCTOR='Хирург';";
     $stmt = $conn->query($query);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $output = "";
-    foreach ($results as $row) {
-        $output .= print_r($row, true) . "\n";
-    }
-
-    echo $output;
+    echo json_encode($results);
 } catch (PDOException $e) {
     echo "Ошибка: " . $e->getMessage();
     die();
 }
+?>

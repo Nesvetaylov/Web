dbc817e917a631afe3c08c3a4777b1cfeaeed4ef
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-23 02:08:04 +0300

    Update first_query.php

--- SummerPractice/QUERIES/first_query.php
+++ SummerPractice/QUERIES/first_query.php
@@ -2,7 +2,6 @@
 
 $v = rand();
 echo "<link rel='stylesheet' type='text/css' href='../style_query_1.css?v=$v' media='screen' />";
-echo "<script>var selectedForm='$selectedForm'</script>"; 
 
 include("../Secret.php");
 $username = username;

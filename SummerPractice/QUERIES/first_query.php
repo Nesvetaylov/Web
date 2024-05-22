38e9fb75d11a8a2d1da0ff1e0da6fb7866f238d2
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-22 22:50:59 +0300

    Add files via upload

---
+++ SummerPractice/QUERIES/first_query.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: application/json; charset=UTF-8');
+
+include("../Secret.php");
+$username = username;
+$password = password;
+
+try {
+    $conn = new PDO(
+        "mysql:host=localhost;dbname=$username",
+        $username,
+        $password,
+        [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+    );
+    $query = select * from DOCTORS where SPECIALITY_DOCTOR='Хирург';
+    $stmt = $db->query($query);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($results);
+} catch (PDOException $e) {
+    echo "Ошибка: " . $e->getMessage();
+    die();
+}
+?>

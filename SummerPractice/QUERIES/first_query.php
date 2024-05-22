72a61ab91f1bc502c94c43e780396419ea7fdf4a
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-22 23:43:21 +0300

    Update first_query.php

--- SummerPractice/QUERIES/first_query.php
+++ SummerPractice/QUERIES/first_query.php
@@ -3,20 +3,22 @@
 include("../Secret.php");
 $username = username;
 $password = password;
+$opt = $_POST['options'];
 
-try {
-    $conn = new PDO( "mysql:host=localhost;dbname=$username", $username, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
-    $query = "select * from DOCTORS where SPECIALITY_DOCTOR='Хирург';";
-    $stmt = $conn->query($query);
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    $output = "";
-    foreach ($results as $row) {
-        $output .= print_r($row, true) . "\n";
+if ($opt=="1") {
+    // текст SQL запроса, который будет передан базе
+    $query = "select * from DOCTORS where SPECIALITY_DOCTOR='Хирург';"
+    
+     // выполняем запрос к базе данных
+     $stmt = $conn->query($query);
+     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+ 
+    // выводим полученные данные
+    while($row = $result->fetch_assoc()){
+      echo  $row['FIO_DOCTOR'] . ' - ' . $row['SPECIALITY_DOCTOR'] . ' - ' . $row['COST_OF_ADMISSION'] . ' - ' . $row['PERCENTAGE_OF_SALARY'] . "<br>";
     }
-
-    echo $output;
-} catch (PDOException $e) {
+  }
+catch (PDOException $e) {
     echo "Ошибка: " . $e->getMessage();
     die();
 }

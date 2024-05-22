613ab4a856a42013527a73f433699357f027aace
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-23 02:19:46 +0300

    Update first_query.php

--- SummerPractice/QUERIES/first_query.php
+++ SummerPractice/QUERIES/first_query.php
@@ -15,7 +15,7 @@ try {
     $stmt = $conn->query($query);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo '<table>';
+    echo '<table class="doctors-table">';
     echo '<tr>';
     echo "<td>Номер доктора</td>";
     echo "<td>ФИО доктора</td>";

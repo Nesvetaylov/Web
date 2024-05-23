b3aeaf6a1c05badfbef0a7b1d90bf37614296b17
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-23 11:34:24 +0300

    Update six_query.php

--- SummerPractice/QUERIES/six_query.php
+++ SummerPractice/QUERIES/six_query.php
@@ -10,7 +10,7 @@ $password = password;
 echo "<h2>Запрос 6: Вычисляет размер заработной платы врача за каждый прием</h2>";
 try {
     $conn = new PDO( "mysql:host=localhost;dbname=$username", $username, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
-    $query = "SELECT d.DOCTOR_ID, d.FIO_DOCTOR, d.SPECIALITY_DOCTOR, aop.DATE, d.COST_OF_ADMISSION, d.PERCENTAGE_OF_SALARY, (d.COST_OF_ADMISSION * d.PERCENTAGE_OF_SALARY) AS Зарплата
+    $query = "SELECT d.DOCTOR_ID, d.FIO_DOCTOR, d.SPECIALITY_DOCTOR, aop.DATE, d.COST_OF_ADMISSION, d.PERCENTAGE_OF_SALARY
     FROM DOCTORS d
     JOIN ADMISSION_OF_PATIENTS aop ON d.DOCTOR_ID = aop.DOCTOR_ID;";
     $stmt = $conn->query($query);
@@ -23,7 +23,7 @@ try {
     echo "<td>Дата приёма</td>";
     echo "<td>Стоимость приёма</td>";
     echo "<td>Процент отчисления</td>";
-    echo "<td>Зарплата</td>";
+    echo "<td>Зарплата за один приём</td>";
     echo '</tr>';
     foreach ($results as $row) {
         echo '<tr>';
@@ -33,7 +33,7 @@ try {
         echo "<td>" . $row['DATE'] . "</td>";
         echo "<td>" . $row['COST_OF_ADMISSION'] . "</td>";
         echo "<td>" . $row['PERCENTAGE_OF_SALARY'] . "</td>";
-        echo "<td>" . $row['Зарплата'] . "</td>";
+        echo "<td>" . $row['COST_OF_ADMISSION'] * $row['PERCENTAGE_OF_SALARY'] / 100 . "</td>";
         echo '</tr>';
     }
     echo '</table>';

51ff6de040e6d5ddf96097a5d921e867f177f66c
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-23 11:40:18 +0300

    Add files via upload

---
+++ SummerPractice/QUERIES/seven_query.php
@@ -0,0 +1,31 @@
+<?php $v = rand(); echo "<link rel='stylesheet' type='text/css' href='../style_query_1.css?v=$v' media='screen' />"; 
+include("../Secret.php"); 
+
+$username = username;
+$password = password;
+
+// Запрос 7: Выполняет группировку по полю Дата приема и вычисляет среднюю стоимость приема
+echo "<h2> Запрос 7: Выполняет группировку по полю Дата приема и вычисляет среднюю стоимость приема</h2>";
+
+try {
+    $conn = new PDO( "mysql:host=localhost;dbname=$username", $username, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
+    $query = "SELECT DATE, AVG(COST_OF_ADMISSION) AS Средняя_стоимость_приема FROM ADMISSION_OF_PATIENTS GROUP BY DATE";
+    $stmt = $conn->query($query);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo '<table class="table">';
+    echo '<tr>';
+    echo "<td>Дата</td>";
+    echo "<td>Средняя стоимость приема</td>";
+    echo '</tr>';
+
+    foreach ($results as $row) {
+        echo '<tr>';
+        echo "<td>" . $row['DATE'] . "</td>";
+        echo "<td>" . $row['Средняя_стоимость_приема'] . "</td>";
+        echo '</tr>';
+    }
+} catch (PDOException $e) {
+    echo "Ошибка: " . $e->getMessage();
+    die();
+}

8410d531b71835413b3dca577550644d96d399bf
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-23 07:41:02 +0300

    Update six_query.php

--- SummerPractice/QUERIES/six_query.php
+++ SummerPractice/QUERIES/six_query.php
@@ -9,9 +9,9 @@ $password = password;
 echo "<h2>Запрос 6: Вычисляет размер заработной платы врача за каждый прием</h2>";
 try {
     $conn = new PDO( "mysql:host=localhost;dbname=$username", $username, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION] );
-    $query = "SELECT d.DOCTOR_ID, d.FIO_DOCTOR, d.SPECIALITY_DOCTOR, a.COST_OF_ADMISSION, d.PERCENTAGE_OF_SALARY, (a.COST_OF_ADMISSION * d.PERCENTAGE_OF_SALARY) AS Зарплата
-              FROM DOCTORS d
-              JOIN ADMISSION_OF_PATIENTS a ON d.DOCTOR_ID = a.DOCTOR_ID";
+    $query = "SELECT d.FIO_DOCTOR, d.SPECIALITY_DOCTOR, a.COST_OF_ADMISSION, d.PERCENTAGE_OF_SALARY,
+    (a.COST_OF_ADMISSION * d.PERCENTAGE_OF_SALARY) AS Зарплата FROM DOCTORS d
+    JOIN ADMISSION_OF_PATIENTS a ON d.DOCTOR_ID = a.DOCTOR_ID";
     $stmt = $conn->query($query);
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo '<table class="table">';
@@ -21,16 +21,14 @@ try {
     echo "<td>Специальность</td>";
     echo "<td>Стоимость приёма</td>";
     echo "<td>Процент отчисления</td>";
-    echo "<td>Зарплата</td>";
     echo '</tr>';
     foreach ($results as $row) {
         echo '<tr>';
-        echo "<td>" . $row['DOCTOR_ID'] . "</td>";
+        echo "<td>" . $row['DOCTOR_ID'] . "</td>"; // adjust this line if DOCTOR_ID is not in the SELECT clause
         echo "<td>" . $row['FIO_DOCTOR'] . "</td>";
         echo "<td>" . $row['SPECIALITY_DOCTOR'] . "</td>";
         echo "<td>" . $row['COST_OF_ADMISSION'] . "</td>";
         echo "<td>" . $row['PERCENTAGE_OF_SALARY'] . "</td>";
-        echo "<td>" . $row['Зарплата'] . "</td>";
         echo '</tr>';
     }
     echo '</table>';

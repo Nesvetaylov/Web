c0bf2291314b06f57c40b537c3cca78def1623fd
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-24 17:20:57 +0300

    Update index.php

--- Task6/index.php
+++ Task6/index.php
@@ -5,8 +5,8 @@ header('Content-Type: text/html; charset=UTF-8');
 include('../Secret.php');
 
 // HTTP Basic Authentication
-$adminUsername = 'admin';
-$adminPassword = 'your_admin_password';
+$adminUsername = username;
+$adminPassword = password;
 
 if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || $_SERVER['PHP_AUTH_USER'] != $username || $_SERVER['PHP_AUTH_PW'] != $password) {
     header('WWW-Authenticate: Basic realm="Admin Area"');

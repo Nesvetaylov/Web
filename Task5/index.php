172ee33cf9a7d460f974bd534020cf10ac3a6e57
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-05-24 21:25:21 +0300

    Update index.php

--- Task5/index.php
+++ Task5/index.php
@@ -101,6 +101,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
 
     $started_session = session_start();
+    $messages[] = '1:'.!empty($_COOKIE[session_name()]) .'<br> :: '. $started_session .'<br> :: '. !empty($_SESSION['entered']).'<br> ';
 
     if (!empty($_COOKIE[session_name()]) &&
         $started_session && !empty($_SESSION['entered'])) {

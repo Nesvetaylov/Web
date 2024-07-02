b6be3e0a4e97f8b8719c3ea9a9a4de9ba8be0238
Nesvetaylov <143995310+Nesvetaylov@users.noreply.github.com>
2024-07-02 11:18:17 +0300

    Update form.php

--- Task7/form.php
+++ Task7/form.php
@@ -117,8 +117,8 @@
 
       <label>
         <input type="checkbox" name="V"
-          <?php if ($errors['V']) {print 'class="error"';} ?>
-          <?php if (!empty($values['V'])) {print "checked";}; ?>
+          <?php if ($errors['check']) {print 'class="error"';} ?>
+          <?php if (!empty($values['check'])) {print "checked";}; ?>
           />
         c контрактом ознакомлен(а)
       </label>
